f5abb2a92a7a163040461e387e98aa075bea976b
Caleb Knight <knightca@oregonstate.edu>
2022-05-07 13:42:32 -0700

    Added in a function to set the location of the button and passed the function instead of multiple variables to draw

--- main.rb
+++ main.rb
@@ -10,13 +10,25 @@ class Button
 end
 
 class DarkMode < Button 
+
+  def set_location(x_coordinate, y_coordinate)
+    @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
+  end
+
   def draw_button
-    paint(label_text, x_coordinate, y_coordinate, foreground_color - 10, '#111111')
+    paint(label_text, set_location(x_coordinate, y_coordinate), foreground_color - 10, '#111111')
   end
 end
 
 class LightMode < Button 
+
+  def set_location(x_coordinate, y_coordinate)
+    @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
+  end
+
   def draw_button
-    paint(label_text, x_coordinate, y_coordinate, foreground_color + 10, '#E0E0E0')
+    paint(label_text, set_location(x_coordinate, y_coordinate), foreground_color + 10, '#E0E0E0')
   end
 end
