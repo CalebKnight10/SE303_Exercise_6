0fe0fbc30d6a2b585a35596ba8a8b4352e3dd647
Caleb Knight <knightca@oregonstate.edu>
2022-05-07 13:36:03 -0700

    Created a parent Button class and two child classes (DarkMode and LightMode)

--- main.rb
+++ main.rb
@@ -1,11 +1,22 @@
-def draw_button(label_text, x_coordinate, y_coordinate, foreground_color, is_dark_mode)
+class Button
+  attr_accessor: :label_text, :x_coordinate, :y_coordinate, :foreground_color
 
+  def initialize
+    @label_text = label_text
+    @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
+    @foreground_color = foreground_color
+  end
 end
 
-def draw_light_button
-  paint(label_text, x_coordinate, y_coordinate, foreground_color + 10, '#E0E0E0')
+class DarkMode < Button 
+  def draw_dark_button
+    paint(label_text, x_coordinate, y_coordinate, foreground_color - 10, '#111111')
+  end
 end
 
-def draw_dark_button
-  paint(label_text, x_coordinate, y_coordinate, foreground_color - 10, '#111111')
+class LightMode < Button 
+  def draw_light_button
+    paint(label_text, x_coordinate, y_coordinate, foreground_color + 10, '#E0E0E0')
+  end
 end
